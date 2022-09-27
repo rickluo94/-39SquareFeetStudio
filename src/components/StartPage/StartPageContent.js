db63efc24cfc6f1adc369f2a8f8d9d365e72ece1
Rick Lo <edcrfvlily5@gmail.com>
2022-09-28 03:47:06 +0800

    [modify]change start content

--- src/components/StartPage/StartPageContent.js
+++ src/components/StartPage/StartPageContent.js
@@ -1,7 +1,7 @@
 import classes from "./StartPageContent.module.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { ReactComponent as SeoImg } from "../../assets/seoimg.svg";
 import { ReactComponent as Cat } from "../../assets/cat.svg";
+import { ReactComponent as Check } from "../../assets/check.svg";
 import wildDonkeyImg from "../../assets/wilddonkeyworks2020.png";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -16,9 +16,7 @@ const StartPageContent = () => {
     dispatch(collectionHide());
   };
 
-  AOS.init({
-    duration: 2000,
-  });
+
 
   return (
     <section className={classes.startBody}>
@@ -26,13 +24,13 @@ const StartPageContent = () => {
         <div className={classes.content}>
           <div className={classes.subject}>
             <h1>
-              Pet 伴
+              山石久平
               <br />
-              創意工作室
+              工作室
               <br />
             </h1>
             <div className={classes.cosH2}>
-              SEO行銷、RWD網頁開發、切版、後端服務、後端介接
+              SEO行銷、RWD網頁開發、切版、後台功能客製化
             </div>
           </div>
           {/*<Cat className={classes.logo} />*/}
@@ -40,12 +38,63 @@ const StartPageContent = () => {
       </div>
       <div className={classes.mainBody}>
         <div className={classes.mainContent}>
-          <div className={classes.provide}>提供服務</div>
+          <div className={classes.mainTitle}>網頁設計服務</div>
+          <div className={classes.ulGrid}>
+            <div className={classes.liGrid}>
+              <h2>網頁設計</h2>
+              <ul>
+                <li><Check className={classes.Check} />一頁式廣吿</li>
+                <li><Check className={classes.Check} />形象網站</li>
+              </ul>
+              <p>提供響應式網頁服務,可相容於手機、平版、電腦之網頁,提供客製化服務及網頁動畫.</p>
+            </div>
+            <div className={classes.liGrid}>
+              <h2>購物網站設計</h2>
+              <ul>
+                <li><Check className={classes.Check} />包含會員管理</li>
+                <li><Check className={classes.Check} />購物車</li>
+                <li><Check className={classes.Check} />後台功能</li>
+              </ul>
+              <p>提供具有導購功能之購物網站設計,會員功能、購物車、結帳流程,後台功能客製。</p>
+            </div>
+            <div className={classes.liGrid}>
+              <h2>RWD網頁切版</h2>
+              <ul>
+                <li><Check className={classes.Check} />HTML / CSS</li>
+                <li><Check className={classes.Check} />Tailwind</li>
+                <li><Check className={classes.Check} />Bootstrap</li>
+              </ul>
+              <p>提供已有設計稿之顧客,協助您進行網頁切版。</p>
+            </div>
+            <div className={classes.liGrid}>
+              <h2>後端開發</h2>
+              <ul>
+                <li><Check className={classes.Check} />ASP.NET</li>
+                <li><Check className={classes.Check} />PHP Laravel</li>
+              </ul>
+              <p>提供現有系統功能修改,新增再開發等服務。</p>
+            </div>
+          </div>
+          {/* 區域1結束 */}
+          <div className={classes.mainTitle}>SEO 服務</div>
+          <div className={classes.seoContainer}>
+            <div>
+              <SeoImg className={classes.seoImg} />
+            </div>
+            <div className={classes.seoCard}>
+              <h2>SEO行銷</h2>
+              <ul>
+                <li><Check className={classes.Check} />百度</li>
+                <li><Check className={classes.Check} />Google</li>
+              </ul>
+              <p>提供SEO文案寫手，有效增加網站曝光率,提升搜尋引擎自然排序</p>
+            </div>
+          </div>
+          {/* 區域2結束 */}
+          <div className={classes.mainTitle}>報價諮詢</div>
           <div className={classes.ulCard}>
-            <div className={classes.liCard} data-aos="fade-up-left">
-              <p>RWD 網站切版</p>
-              <p>NT$ 800 </p>
-              <p>起/頁</p>
+            <div className={classes.liCard} >
+              <h2>RWD 網站切版</h2>
               <ul>
                 <li>HTML + CSS</li>
                 <li>Tailwind</li>
@@ -62,12 +111,9 @@ const StartPageContent = () => {
                 </a>
               </div>
             </div>
-            <div className={classes.liCard} data-aos="fade-up-right">
-              <p>後端服務</p>
-              <p>NT$ 1500</p>
-              <p>起</p>
+            <div className={classes.liCard} >
+              <h2>後端服務</h2>
               <ul>
-                <li>Restful API</li>
                 <li>ASP.NET</li>
                 <li>PHP Laravel</li>
               </ul>
@@ -82,13 +128,11 @@ const StartPageContent = () => {
                 </a>
               </div>
             </div>
-            <div className={classes.liCard} data-aos="fade-up-left">
-              <p>後端介接</p>
-              <p>NT$ 1500 </p>
-              <p>起</p>
+            <div className={classes.liCard} >
+              <h2>後端介接</h2>
               <ul>
                 <li>React</li>
-                <li>ASP.NET 、 WPF</li>
+                <li>ASP.NET </li>
                 <li>Laravel</li>
               </ul>
               <div className={classes.growArea}>
@@ -102,14 +146,11 @@ const StartPageContent = () => {
                 </a>
               </div>
             </div>
-            <div className={classes.liCard} data-aos="fade-up-right">
-              <p>SEO行銷</p>
-              <p>NT$ 50000</p>
-              <p>起／月</p>
+            <div className={classes.liCard} >
+              <h2>SEO行銷</h2>
               <ul>
                 <li>百度 SEO</li>
                 <li>Google SEO</li>
-                <li>百度黑帽 SEO</li>
               </ul>
               <div className={classes.growArea}>
                 <a
